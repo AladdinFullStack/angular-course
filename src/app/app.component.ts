7917f0cafe780d8870328844649ce93ff8a113c3
Chris Min <suntsu777@gmail.com>
2021-05-04 17:14:04 -0400

    angular pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import {Course} from './model/course'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = COURSES;
 
+  courses = COURSES;
+  title = this.courses[0].description;
+  price = 9.9934123423423;
+  rate = 0.67;
+  course = COURSES[0];
+  startDate = new Date(2000,0,1)
   onCardClicked() {
     console.log("App component - click event from course-card component bubbled up..........")
   }
